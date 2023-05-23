471fc96e7d257fe98b8683bf10533392afc0fb88
Joab Ramirez <kennet.guzman.ramirez@gmail.com>
2023-05-22 22:00:57 -0600

    Se hacen editables las celdas, AUN NO ESTAN ENLAZADAS

--- src/vista/VistaExcelPrincipalController.java
+++ src/vista/VistaExcelPrincipalController.java
@@ -6,11 +6,14 @@ package vista;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
 
 /**
  * FXML Controller class
@@ -30,15 +33,17 @@ public class VistaExcelPrincipalController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // Crear las columnas dinámicamente
-    for (char c = 'A'; c <= 'Z'; c++) {
-        TableColumn<String, String> column = new TableColumn<>(String.valueOf(c));
-        tblExcel.getColumns().add(column);
+        for (char c = 'A'; c <= 'Z'; c++) {
+            TableColumn<String, String> column = new TableColumn<>(String.valueOf(c));
+            column.setCellFactory(TextFieldTableCell.forTableColumn());
+            column.setEditable(true);
+            tblExcel.getColumns().add(column);
+        }
+
+        // Crear las filas enumeradas
+        for (int i = 1; i <= 100; i++) {
+            tblExcel.getItems().add("");
+        }
     }
 
-    // Crear las filas enumeradas
-    for (int i = 1; i <= 100; i++) {
-        tblExcel.getItems().add(String.valueOf(i));
-    }
-    }    
-    
 }
